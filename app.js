2196a0b6ff8be6e896cebe27437c60c21abaccc9
SungJun Kim <98739079+alltimeno1@users.noreply.github.com>
2022-03-14 18:49:00 +0900

    first commit

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.delete('/api/members/:id', async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening...');
 });
